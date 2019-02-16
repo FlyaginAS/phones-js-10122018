ed9edad46825b8ecdeafcece1a5f4b083eb1ca77
Alexander Flyagin <rus.reg.36@gmail.com>
2019-02-16 17:12:49 +0300

    1

--- app/app.js
+++ app/app.js
@@ -1,11 +1,6 @@
 import {PhonesPageComponent} from './phones-page/phones-page.component.js';
 
- let page = new PhonesPageComponent({
+new PhonesPageComponent({
   element: document.querySelector('#root')
 });
- document.body.addEventListener('click', function (event) {
-     if(event.target.closest('.back')) {
-         page._phoneViewer.hide();
-         page._phoneCatalog.show();
-     }
- });
+
